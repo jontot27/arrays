ed05cc0f55967de387b5f6a2924c9d3d97595fba
jontot27 <j.a.ballesterosjr@gmail.com>
2024-06-19 07:19:35 +0800

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let firstName = ["Luffy", "Nami", "Chopper", "Zoro", "Sanji"];
+let lastName = ["Monkey", "The Navigator", "Tony Tony", "Roronoa", "The Cook" ];
+let age = [16, 15, 12, 18, 17]
+
+alert(`First Name = ${firstName[0]}
+Last Name = ${lastName[0]}
+Age = ${age[0]}`);
